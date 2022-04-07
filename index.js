4fa83bfa1c70f01cfe3acf8b4f12aab9201c2fc2
Kevin <programador.kevin4773@gmail.com>
2022-04-07 13:25:34 -0300

    Completando a API e adicionando as modelagens de livro

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-// Importa o pacote Express
-const express = require('express')
+/* IMPORTAÇÕES DE PACOTES */
+const express = require('express');
 
-// Instancia o pacote Express
+/* INSTANCIAS DE PACOTES */
+//express:
 const app = express();
 
-// Configuração do express para manipulação do formato JSON
+/* CONFIGURA O EXPRESS PARA LIDAR COM DADOS NO FORMATO JSON */
 app.use(express.json());
-app.use(express.urlencoded({
-    
-    extended: true
+app.use(express.urlencoded({ extended: true }));
 
-}));
-
-// Importa a controller de usuario
-const usuarioController = require('./src/controllers/UsuarioController');
+/* IMPORTA E CONFIGURA OS ARQUIVOS DE ROTAS DE USUÁRIOS */
+const usuarioController = require('./controller/UsuarioController');
 app.use('/', usuarioController);
 
-// Chama o arquivo de model
-const usuario = require('./src/models/Usuario');
+/* IMPORTA E CONFIGURA OS ARQUIVOS DE ROTAS DE LIVROS*/
+const livroController = require('./controller/LivroController');
+app.use('/', livroController);
 
-// Servidor auto-contido do Express
-// Recebe as requisições e devolve as respostas
-// Paramentro de Listen
-// 1- PORTA LÓGICA
-// 2- CALLBACK
-const port = 3000;
-
-app.listen(port, () => console.log(`Estamos online na porta: ${port}!`));
+/* INSTANCIA DO SERVIDOR (express) */
+app.listen(3000, ()=>{ 
+    console.log('SERVIDOR RODANDO NA URL: http://localhost:3000'); 
+});
